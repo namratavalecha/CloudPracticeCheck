161dbf523e4f1990a0b2cf8052ce7362ccefa811
namratavalecha <namratavalecha6@gmail.com>
2021-09-30 01:25:57 +0530

    first commit

---
+++ Models/CartItem.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApi.Models
+{
+    public class CartItem
+    {
+
+        //changed the cart item--added this line
+        [Key]
+        public int Id{ get; set; }
+
+        public int UserId{ get; set; }
+
+        public int MenuItemId{ get; set; }
+
+        public string  MenuItemName { get; set; }
+    }
+}
